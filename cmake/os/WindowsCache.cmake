MariaDB/server

commit 0ee5cf837e3a0464acc20db2a2aee0adaff3f2ac

disks plugin: check for build prerequisites properly

plugin/disks/CMakeLists.txt
@@ -1,5 +1,7 @@
-IF("${CMAKE_SYSTEM}" MATCHES "Linux")
+INCLUDE (CheckIncludeFiles)
+CHECK_INCLUDE_FILES ("sys/statvfs.h;mntent.h" INFO_HEADERS LANGUAGE CXX)
+
+IF (INFO_HEADERS)
   INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/sql)
   MYSQL_ADD_PLUGIN(DISKS information_schema_disks.cc MODULE_ONLY RECOMPILE_FOR_EMBEDDED)
 ENDIF()
-

rawbertp/mariadb

commit 7b4cdc5a0044a3ae5adcf789c27d1f4c3450392d

disks plugin: getmntent_r required to build properly
Without this it fails on AIX and possibly some BSDs.

Fixes: 0ee5cf837e3a0464acc20db2a2aee0adaff3f2ac

cmake/os/WindowsCache.cmake
@@ -64,6 +64,7 @@ SET(HAVE_GETHOSTBYADDR_R CACHE  INTERNAL "")
 SET(HAVE_GETHRTIME CACHE  INTERNAL "")
 SET(HAVE_GETPAGESIZE CACHE  INTERNAL "")
 SET(HAVE_GETPASS CACHE  INTERNAL "")
+SET(HAVE_GETMNTENT_R CACHE  INTERNAL "")
 SET(HAVE_GETPASSPHRASE CACHE  INTERNAL "")
 SET(HAVE_GETPWNAM CACHE  INTERNAL "")
 SET(HAVE_GETPWUID CACHE  INTERNAL "")

plugin/disks/CMakeLists.txt
@@ -1,7 +1,8 @@
 INCLUDE (CheckIncludeFiles)
 CHECK_INCLUDE_FILES ("sys/statvfs.h;mntent.h" INFO_HEADERS LANGUAGE CXX)
 
-IF (INFO_HEADERS)
+CHECK_SYMBOL_EXISTS (getmntent_r "mntent.h" HAVE_GETMNTENT_R)
+IF (INFO_HEADERS AND HAVE_GETMNTENT_R)
   INCLUDE_DIRECTORIES(${CMAKE_SOURCE_DIR}/sql)
   MYSQL_ADD_PLUGIN(DISKS information_schema_disks.cc MODULE_ONLY RECOMPILE_FOR_EMBEDDED)
 ENDIF()